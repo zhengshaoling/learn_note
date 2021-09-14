9df6df9f33db9d1236a908335d3d47cff509b953
00 <602761930@qq.com>
2021-09-14 14:35:22 +0800

    优化地图

--- nuxt-seo/pages/map.vue
+++ nuxt-seo/pages/map.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: your name
  * @Date: 2021-08-09 16:51:23
- * @LastEditTime: 2021-09-13 17:01:44
- * @LastEditors: linglingling
+ * @LastEditTime: 2021-09-14 14:33:59
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \nuxt-seo\pages\map.vue
 -->
@@ -15,14 +15,12 @@
 import * as echarts from 'echarts'
 const mapJSon = require('~/assets/json/map.json')
 const mapSereisData = require('~/assets/json/mapSeriesData.json');
-// import mapJSon from '~/assets/json/map.json'
 export default {
         name: 'Map',
         data() {
                 return {
                        myChart: null,
-                       nowIndex: -1,
-                       myTime: null
+                       nowIndex: -1
                 }
         },
         mounted() {
@@ -45,25 +43,25 @@ export default {
                         return defalultColor;
                 },
                 initChart() {
+                        let that = this;
                         const geoJSON = {
-                                '100000': mapJSon
+                                'china': mapJSon
                         };
 
-                        echarts.registerMap('100000', { geoJSON: geoJSON['100000'] });
+                        echarts.registerMap('china', { geoJSON: geoJSON['china'] });
                         this.myChart = echarts.init(document.getElementById('Map'));
                         let option = {
-                           geo: { // 作为底图，设置地图外围边框
-                                map: '100000',
-                                itemStyle: {
-                                        areaColor: 'rgba(255,255,255,0)',
-                                        borderColor: '#28cccc',
-                                        borderWidth: 1,
-                                }
+                                geo: { // 作为底图，设置地图外围边框
+                                        map: 'china',
+                                        itemStyle: {
+                                                areaColor: 'rgba(255,255,255,0)',
+                                                borderColor: '#28cccc',
+                                                borderWidth: 1,
+                                        },
                                 },
-                                series: [
-                                {
-                                        type: 'map',
-                                        map: '100000',
+                                series: [{
+                                        type: 'scatter',
+                                        coordinateSystem: 'geo',
                                         label: {
                                                 normal: {
                                                         show: false,
@@ -101,15 +99,42 @@ export default {
                                                         borderColor: '#8bdbff',
                                                 }
                                         },
-                                        data: mapSereisData.list
+                                        data: that.convertScatterData(mapSereisData.list)
                                 }],
-                                
-                                tooltip: this.getTooltip()     
+                                visualMap: {
+                                        type: "continuous",
+                                        min: 0,
+                                        max: 1200,
+                                        circulable: true,
+                                        inRange: {
+                                                color: ['#50a3ba', '#eac736', '#d94e5d']
+                                        },
+                                        left: '2px',
+                                        // 配置文字样式
+                                        textStyle: {
+                                                color: "#fff",
+                                                fontSize: "12px",
+                                        }
+                                },
+                                tooltip: this.getTooltip()  
                         }
                         this.myChart.setOption(option);
                         setTimeout(()=>{
                                 this.autoLoopTooTip();
                         }, 800)
+                },
+                convertScatterData(data) {
+                        let res = [];
+                        for(let i = 0; i < data.length; i++) {
+                                let geoCoord = mapSereisData.geo[data[i].name];
+                                if(geoCoord) {
+                                        res.push({
+                                                name: data[i].name,
+                                                value: geoCoord.concat(data[i].value)
+                                        });
+                                }
+                        }
+                        return res;
                 },
                 autoLoopTooTip() {
                         let that = this;
@@ -143,20 +168,18 @@ export default {
                       return {
                                 show: true,
                                 formatter: function (params) {
-                                        debugger;
                                         if (params.value != params.value) return "";
                                         let f = '<div style="line-height: 30px; width: 110px; font-weight: bold; margin: 10px 0 10px 20px;"><div style="color: #fff; font-size: 24px; border-bottom: 1px solid #fff;">' +
                                                 params.name + 
                                                 '</div>' + 
                                                 '<div style="color: #ffea00; font-size: 16px;">数量：' +
-                                                params.value +
+                                                params.value[2] +
                                                 '</div></div>'
                                         return f;
                                 },
                                 position: 'right',
                                 backgroundColor: 'rgba(255,255,255,0)',
-                        
-                                extraCssText: 'width: 180px; height: 100px; background-image: url(/img/center/tooltip.png); background-size: cover; background-repeat: no-repeat;'
+                                extraCssText: 'width: 180px; height: 100px; background-image: url(/img/center/tooltip.png); background-size: cover; background-repeat: no-repeat; border: none;'
                         }  
                 }
         }
